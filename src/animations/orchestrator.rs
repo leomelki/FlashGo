595f29b0115a9338ab97b0a8a44e75499d20007b
leomelki <leo.melki@datadoghq.com>
2025-05-21 18:40:07 +0000

    fix esp build

--- src/animations/orchestrator.rs
+++ src/animations/orchestrator.rs
@@ -1,8 +1,7 @@
 use crate::drivers::ble::Service;
 use crate::drivers::driver;
 use crate::drivers::sync::SyncTrait;
-use crate::protos::animations_::list_::rainbow_::RainbowAnimation;
-use crate::protos::animations_::{SetAnimation, SetAnimation_};
+use crate::protos::animations_::SetAnimation;
 use crate::protos::bpm_::SetBPM;
 use crate::sync::PartialDeviceState;
 use crate::{
@@ -12,14 +11,13 @@ use crate::{
 };
 use anyhow::Result;
 use ble::Characteristic;
-use micropb::{MessageDecode, MessageEncode, PbDecoder, PbEncoder};
-use std::vec::Vec;
+use micropb::{MessageDecode, PbDecoder};
 
 pub struct AnimationsOrchestrator<S, T>
 where
     S: Service,
     <S as Service>::Characteristic: Send + Sync + 'static,
-    T: SyncTrait + Send + Sync + 'static,
+    T: SyncTrait + 'static,
 {
     animation_characteristic: <S as Service>::Characteristic,
     bpm_characteristic: <S as Service>::Characteristic,
@@ -32,7 +30,7 @@ impl<S, T> AnimationsOrchestrator<S, T>
 where
     S: Service,
     <S as Service>::Characteristic: Send + Sync + 'static,
-    T: SyncTrait + Send + Sync + 'static,
+    T: SyncTrait + 'static,
 {
     pub fn new(
         mut ble_service: S,
@@ -69,45 +67,58 @@ where
             }
         });
 
-        self.animation_characteristic.set_callback(move |value| {
-            let mut set_animation = SetAnimation::default();
-            let mut decoder = PbDecoder::new(value);
+        let (sender, receiver) = futures::channel::mpsc::channel::<PartialDeviceState>(16);
 
-            if let Err(e) = set_animation.decode(&mut decoder, value.len()) {
-                log::error!("AnimationOrchestrator received invalid animation: {:?}", e);
-                return Ok(());
-            }
+        {
+            let sender_clone = sender.clone();
+            self.animation_characteristic.set_callback(move |value| {
+                let mut set_animation = SetAnimation::default();
+                let mut decoder = PbDecoder::new(value);
 
-            if let Some(animation) = set_animation.animation {
-                log::info!(
-                    "AnimationOrchestrator received BLE animation: {:?}",
-                    animation
-                );
+                if let Err(e) = set_animation.decode(&mut decoder, value.len()) {
+                    log::error!("AnimationOrchestrator received invalid animation: {:?}", e);
+                    return Ok(());
+                }
 
-                self.devices_syncer
-                    .partial_state_update(&PartialDeviceState {
+                if let Some(animation) = set_animation.animation {
+                    log::info!(
+                        "AnimationOrchestrator received BLE animation: {:?}",
+                        animation
+                    );
+
+                    let mut sender = sender_clone.clone();
+                    if let Err(e) = sender.try_send(PartialDeviceState {
                         animation: Some(animation),
                         ..Default::default()
-                    });
-            } else {
-                log::error!("AnimationOrchestrator received invalid animation (no animation)");
-            }
-            Ok(())
-        });
+                    }) {
+                        log::error!("AnimationOrchestrator failed to update animation: {:?}", e);
+                    }
+                } else {
+                    log::error!("AnimationOrchestrator received invalid animation (no animation)");
+                }
+                Ok(())
+            });
+        }
 
-        self.bpm_characteristic.set_callback(move |value| {
-            let mut set_bpm = SetBPM::default();
-            let mut decoder = PbDecoder::new(value);
-            set_bpm.decode(&mut decoder, value.len()).unwrap();
+        {
+            let sender_clone = sender.clone();
+            self.bpm_characteristic.set_callback(move |value| {
+                let mut set_bpm = SetBPM::default();
+                let mut decoder = PbDecoder::new(value);
+                set_bpm.decode(&mut decoder, value.len()).unwrap();
 
-            log::info!("AnimationOrchestrator received BLE bpm: {:?}", set_bpm);
-            self.devices_syncer
-                .partial_state_update(&PartialDeviceState {
+                log::info!("AnimationOrchestrator received BLE bpm: {:?}", set_bpm);
+                let mut sender = sender_clone.clone();
+                if let Err(e) = sender.try_send(PartialDeviceState {
                     bpm: Some(set_bpm.bpm as u16),
                     ..Default::default()
-                });
-            Ok(())
-        });
+                }) {
+                    log::error!("AnimationOrchestrator failed to update bpm: {:?}", e);
+                }
+                Ok(())
+            });
+        }
+
         if self.master {
             self.init_master_orchestrator().await;
         }
@@ -116,10 +127,6 @@ where
     }
 
     async fn init_master_orchestrator(&'static self) {
-        driver::run_async(async move {
-            //todo master things
-            Ok(())
-        })
-        .await;
+        driver::run_async(async move { Ok(()) }).await;
     }
 }
